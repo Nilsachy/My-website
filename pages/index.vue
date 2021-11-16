02dbd88939caf0b37faf0d109c6ca4c30bb2a8ac
Nils Achy <n.c.achy-1@student.tudelft.nl>
2021-11-16 17:16:16 +0100

    Created website

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,101 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container class="center-screen">
+    <v-row>
+      <v-img src="/avatar.jpg" class="avatar" max-height="200" max-width="200" />
+      <v-col cols="12">
+        <h1 class="name">
+          Nils Achy
+        </h1>
+      </v-col>
+      <v-col cols="12">
+        <v-btn
+          id="linkedIn"
+          elevation="2"
+          class="btn mr-2"
+          outlined
+          x-large
+          href="https://www.linkedin.com/in/nils-achy-8a2984195/"
+          target="_blank"
+        >
+          LinkedIn
+        </v-btn>
+        <v-btn
+          id="github"
+          elevation="2"
+          class="btn ml-2"
+          outlined
+          x-large
+          href="https://github.com/Nilsachy"
+          target="_blank"
+        >
+          Github
+        </v-btn>
+      </v-col>
+      <v-col cols="12" class="mt-10">
+        <a class="links" href="mailto:nils.achy@gmail.com">nils.achy@gmail.com</a>
+      </v-col>
+      <v-col cols="12">
+        <a class="links">+33 (0)6.65.06.97.68</a>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  layout: 'simpleview'
+}
+</script>
+
+<style>
+.center-screen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 100vh;
+  margin-top: -50px;
+}
+
+.avatar {
+  margin: auto;
+  width: 50%;
+  border-radius: 100px;
+}
+
+.name {
+  font-size: 100px;
+  color: rgb(105, 105, 141);
+  font-family: 'Trebuchet MS';
+}
+
+.btn {
+  width: 130px;
+  border-width: 2px;
+}
+
+#linkedIn {
+  border-color: #0072b1;
+  color: #0072b1;
+}
+
+#linkedIn:hover {
+  background-color: #0072b1;
+  color: white;
+}
+
+#github {
+  border-color: #c9510c;
+  color: #c9510c;
+}
+
+#github:hover {
+  background-color: #c9510c;
+  color: white;
+}
+
+.links {
+  color: rgb(105, 105, 141)  !important;
+}
+
+</style>
